c8c98e2181cf2a19dbc71858e36c28307bbd7b12
Ashraf Ali Ansari <ali.ansariashraf0341@gmail.com>
2020-11-01 23:04:39 +0530

    creating repo

--- hello_world_app/models.py
+++ hello_world_app/models.py
@@ -1,10 +1,6 @@
 from django.db import models
 
-# Create your models here.
-# class User(models.Model):
-#     name = models.CharField(max_length=70)
-#     email = models.EmailField(max_length=70)
-#     password = models.CharField(max_length = 100)
+
 
 class data(models.Model):
     name = models.CharField(max_length=300,null=True)
@@ -18,7 +14,4 @@ class data(models.Model):
     classification_l4 = models.CharField(max_length=300,null=True)
     image_url = models.CharField(max_length=300,null=True)
 
-# class Document(models.Model):
-#     description = models.CharField(max_length=255, blank=True)
-#     document = models.FileField(upload_to='documents/')
-#     uploaded_at = models.DateTimeField(auto_now_add=True)
+
